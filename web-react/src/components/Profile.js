d5c83ad6de2784f2765e7cd8a8490e5787bf83db
yaonn <tingyaoan@gmail.com>
2020-10-16 11:43:26 +0800

    Use Auth protecting routes
    https://auth0.com/blog/complete-guide-to-react-user-authentication/#Protecting-Routes

--- web-react/src/components/Profile.js
+++ web-react/src/components/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
+import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react'
 import { Avatar, Paper, Typography } from '@material-ui/core'
 
 const Profile = () => {
@@ -14,4 +14,4 @@ const Profile = () => {
     )
   )
 }
-export default Profile
+export default withAuthenticationRequired(Profile)
