1ca321ff2e2d43325293dbb542e070941579a2e6
yaonn <tingyaoan@gmail.com>
2020-10-22 13:57:13 +0800

    Can use local server and desplay data on react
    add query for usercount in schema
    show img in rpi-react

--- rpi-react/src/components/qrcode.js
+++ rpi-react/src/components/qrcode.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import { useQuery, gql } from '@apollo/client'
 // import Image from 'material-ui-image'
-import { Paper } from '@material-ui/core'
+import { Card } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => {
   return {
@@ -32,13 +32,11 @@ export default function Qrcode() {
   const { loading, error, data } = useQuery(GET_QRCODE)
   if (error) return <p>Sign in to view</p>
   return (
-    <Paper className={classes.root}>
+    <Card className={classes.root}>
+      <img src={loading ? '' : data.qrcode} alt="qrcode" />
       <Typography component="p" variant="h4">
-        {loading ? 'Loading...' : data.qrcode}
+        {loading ? 'Loading...' : 'scan to login'}
       </Typography>
-      <Typography color="textSecondary" className={classes.depositContext}>
-        qrcode found
-      </Typography>
-    </Paper>
+    </Card>
   )
 }
