8b0ca5d4118ceebab916e4bdcd0d7c48f45a9a19
yaonn <tingyaoan@gmail.com>
2020-10-27 15:08:34 +0800

    protect schema with directive
    some data can be query only after user login
    use isAuthenticated schema directive
    use apollo-boost for apollo client
    and @apollo/react-hooks for apollo provider

--- web-react/src/index.js
+++ web-react/src/index.js
@@ -1,25 +1,58 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
-import { Auth0Provider } from '@auth0/auth0-react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react'
 
-const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_URI || '/graphql',
-  cache: new InMemoryCache(),
-})
+const AppWithApollo = () => {
+  const [accessToken, setAccessToken] = useState()
+  const { getAccessTokenSilently, loginWithRedirect } = useAuth0()
+
+  const getAccessToken = useCallback(async () => {
+    try {
+      const token = await getAccessTokenSilently()
+      console.log(token)
+      setAccessToken(token)
+    } catch (err) {
+      //loginWithRedirect()
+    }
+  }, [getAccessTokenSilently, loginWithRedirect])
+
+  useEffect(() => {
+    getAccessToken()
+  }, [getAccessToken])
+
+  const client = new ApolloClient({
+    uri: process.env.REACT_APP_GRAPHQL_URI || '/graphql',
+    request: async (operation) => {
+      if (accessToken) {
+        operation.setContext({
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+      }
+    },
+  })
+
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  )
+}
 
 const Main = () => (
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     redirectUri={window.location.origin}
+    audience="https://dev-rlr3ppnt.us.auth0.com/api/v2/"
   >
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <AppWithApollo />
   </Auth0Provider>
 )
 
